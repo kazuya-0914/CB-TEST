e20cecd63d6d7afadc51054df8673728b15f46de
kazuya-0914 <kazu.work.0914@gmail.com>
2023-12-19 22:10:26 +0900

    12/19 PM コミット

--- golf.php
+++ golf.php
@@ -4,6 +4,9 @@ add_shortcode('roles_service', 'roles_service_shortcode');
 
 // 専用ショートコードの実行内容
 function roles_service_shortcode($atts) {
+    // タイムゾーンを日本に設定
+    date_default_timezone_set('Asia/Tokyo');
+
     // 現在ログイン中のユーザーIDを取得
     $user_id = get_current_user_id();
 
@@ -17,11 +20,7 @@ function roles_service_shortcode($atts) {
     $user_roles = $user_info->roles; // 全ての権限の配列（「購読者 + プレムアム」など）
 
     // もし一つ目の権限が購読者であれば二つ目の権限を出力
-    if($user_roles[0] === 'subscriber' ) {
-        $user_role = $user_roles[1];
-    } else {
-        $user_role = $user_roles[0];
-    }
+    $user_role = $user_roles[0] === 'subscriber' ? $user_roles[1] : $user_roles[0];
 
     /* --- 唯一のユニーク値であるメールアドレス + 予約が承認済みのデータを検索 --- */
     // $wpdbクエリ
@@ -41,48 +40,50 @@ function roles_service_shortcode($atts) {
     // SQL文の結果を出力
     $results = $wpdb->get_results($sql, OBJECT);
 
-    // SQL文の結果から予約NG判定（予約データがなければスルー）
-    if(!empty($results)){
-        foreach ($results as $value) {
-            /* --- 【NG1】 マンスリー２、マンスリー4の予約回数制限--- */
-            if ($user_role === 'monthly2' || $user_role === 'monthly4') {
-                // 予約回数カウント
-                $i = 0;
+    /* SQL文の結果から予約NG判定 */
+    /* 予約データが存在かつ、プレミアム会員以外であれば予約NGチェック */
+    if (!empty($results) && $user_role !== 'premium') {
 
-                // Max予約回数
-                if ($user_role === 'monthly2') {
-                    $max_num = 3;
-                } else {
-                    $max_num = 5;
-                }
+        /* --- 【NG1】 マンスリー２、マンスリー4の予約回数制限--- */
+        if ($user_role === 'monthly2' || $user_role === 'monthly4') {
+            // 予約回数カウント
+            $i = 0;
 
+            // Max予約回数
+            $max_num = $user_role === 'monthly2' ? 2 : 4;
+        
+            // 予約データチェック
+            foreach ($results as $value) {
                 // 今月の予約であれば予約回数カウントプラス
                 if (date("Y/m", $value->scheduleUnixTime) === date("Y/m", time())) {
                     $i++;
                 }
 
                 // Max予約回数を超えたら予約不可
-                if ($i > $max_num) {
+                if ($i >= $max_num) {
                     echo "<p style=\"color: red; font-weight: bold;\">{$last_name}{$first_name}さんは今月の予約回数を超えました</p>";
                     // ショートコードを実行して予約不可カレンダーを表示
-                    return do_shortcode("[booking_package id=1 services=13]");
-                } else {
-                    echo "<p>{$last_name}{$first_name}さんは今月{$i}回予約をしています</p>";
+                    return do_shortcode("[booking_package id=1 services=25]");
                 }
             }
+            echo "<p>{$last_name}{$first_name}さんは今月{$i}回予約をしています</p>";
+        }
+
+        // 予約データチェック
+        foreach ($results as $value) {
             
             /* --- 【NG2】現在の時刻が予約時間 + 90分より前だった場合 --- */
             if ($value->scheduleUnixTime + 90 * 60 > time()) {
                 echo "<p style=\"color: red; font-weight: bold;\">{$last_name}{$first_name}さんは現在予約済みです</p>";
                 // ショートコードを実行して予約不可カレンダーを表示
-                return do_shortcode("[booking_package id=1 services=13]");
+                return do_shortcode("[booking_package id=1 services=25]");
             }
     
             /* --- 【NG3】本日予約済みだった場合 --- */
             if (date("Y/m/d", $value->scheduleUnixTime) === date("Y/m/d", time())) {
                 echo "<p style=\"color: red; font-weight: bold;\">{$last_name}{$first_name}さんは本日予約済みです</p>";
                 // ショートコードを実行して予約不可カレンダーを表示
-                return do_shortcode("[booking_package id=1 services=13]");
+                return do_shortcode("[booking_package id=1 services=25]");
             }
         }
     }
